a23dd330779d5f6cb779b2427cf1b806951f6a42
Maria Victoria <marivibilbaog@gmail.com>
2021-10-27 21:02:03 +0200

    Agrego hasta el punto 6 los ejercicios

--- week-1/mandatory/classes-db/Homework-1.sql
+++ week-1/mandatory/classes-db/Homework-1.sql
@@ -29,7 +29,7 @@ cyf_classes(# );
     "mentors_pkey" PRIMARY KEY, btree (id)
 
 --3.- Añadir información a la tabla mentors:
-INSERT INTO mentors (name, yearsinglasgow, address, favprogramlanguage) VALUES ('Maria Victoria Bilbao', 0, 'Barcelona, España', 'JavaScript');
+INSERT INTO mentors (name, yearsinglasgow, address, favprogramlanguage) VALUES ('Maria Victoria Bilbao', 0, 'Barcelona', 'JavaScript');
 
 -- Comprobación de si la información se cargo:
 SELECT * FROM mentors;
@@ -38,11 +38,11 @@ SELECT * FROM mentors;
   1 | Maria Victoria Bilbao |              0 | Barcelona, España | JavaScript
 (1 fila)
 
-INSERT INTO mentors (name, yearsinglasgow, address, favprogramlanguage) VALUES ('Nancy Gonzalez', 1, 'Av. Rómulo Gallegos, Caracas. Venezuela', 'Ninguno');
+INSERT INTO mentors (name, yearsinglasgow, address, favprogramlanguage) VALUES ('Nancy Gonzalez', 1, 'Av. Romulo Gallegos, Caracas. Venezuela', 'JavaScript');
 
-INSERT INTO mentors (name, yearsinglasgow, address, favprogramlanguage) VALUES ('Jesús Cepeda', 20, '39 Dalmally St, Glasgow G20 6RY, Reino Unido', 'PHP');
+INSERT INTO mentors (name, yearsinglasgow, address, favprogramlanguage) VALUES ('Jesus Cepeda', 20, '39 Dalmally St, Glasgow G20 6RY, Reino Unido', 'PHP');
 
-INSERT INTO mentors (name, yearsinglasgow, address, favprogramlanguage) VALUES ('Ruvi Soria', 5, 'Calle de Hortensia 15, Barcelona. España', 'Python');
+INSERT INTO mentors (name, yearsinglasgow, address, favprogramlanguage) VALUES ('Ruvi Soria', 5, 'Calle de Hortensia 15, Barcelona.', 'Python');
 
 INSERT INTO mentors (name, yearsinglasgow, address, favprogramlanguage) VALUES ('Wendy Andrade', 12, '135 Cortmalaw Cres, Glasgow G33 1TD, Regne Unit', 'JavaScript');
 
@@ -50,10 +50,10 @@ INSERT INTO mentors (name, yearsinglasgow, address, favprogramlanguage) VALUES (
 SELECT * FROM mentors;
  id |         name          | yearsinglasgow |                     address                     | favprogramlanguage
 ----+-----------------------+----------------+-------------------------------------------------+--------------------
-  1 | Maria Victoria Bilbao |              0 | Barcelona, España                               | JavaScript
-  2 | Nancy Gonzalez        |              1 | Av. Rómulo Gallegos, Caracas. Venezuela         | Ninguno
-  3 | Jesús Cepeda          |             20 | 39 Dalmally St, Glasgow G20 6RY, Reino Unido    | PHP
-  4 | Ruvi Soria            |              5 | Calle de Hortensia 15, Barcelona. España        | Python
+  1 | Maria Victoria Bilbao |              0 | Barcelona                                       | JavaScript
+  2 | Nancy Gonzalez        |              1 | Av. Romulo Gallegos, Caracas. Venezuela         | JavaScript
+  3 | Jesus Cepeda          |             20 | 39 Dalmally St, Glasgow G20 6RY, Reino Unido    | PHP
+  4 | Ruvi Soria            |              5 | Calle de Hortensia 15, Barcelona.               | Python
   5 | Wendy Andrade         |             12 | 135 Cortmalaw Cres, Glasgow G33 1TD, Regne Unit | JavaScript
 (5 filas)
 
@@ -62,16 +62,13 @@ INSERT INTO mentors (name, yearsinglasgow, address, favprogramlanguage) VALUES (
 SELECT * FROM mentors;
  id |         name          | yearsinglasgow |                     address                     |   favprogramlanguage   
 ----+-----------------------+----------------+-------------------------------------------------+-------------------------
-  1 | Maria Victoria Bilbao |              0 | Barcelona, España                               | JavaScript
-  2 | Nancy Gonzalez        |              1 | Av. Rómulo Gallegos, Caracas. Venezuela         | Ninguno
-  3 | Jesús Cepeda          |             20 | 39 Dalmally St, Glasgow G20 6RY, Reino Unido    | PHP
-  4 | Ruvi Soria            |              5 | Calle de Hortensia 15, Barcelona. España        | Python
+  1 | Maria Victoria Bilbao |              0 | Barcelona                                       | JavaScript
+  2 | Nancy Gonzalez        |              1 | Av. Romulo Gallegos, Caracas. Venezuela         | JavaScript
+  3 | Jesus Cepeda          |             20 | 39 Dalmally St, Glasgow G20 6RY, Reino Unido    | PHP
+  4 | Ruvi Soria            |              5 | Calle de Hortensia 15, Barcelona.               | Python
   5 | Wendy Andrade         |             12 | 135 Cortmalaw Cres, Glasgow G33 1TD, Regne Unit | JavaScript
   6 | Pedro Bilbao          |             60 | 119 Cortmalaw Cres, Glasgow G33 1TD, Regne Unit | JavaScript, PHP, Python
   (6 filas)
-
--- Modificar un dato de un mentor:
-
 
 
 --4.- Creamos tabla llamada students:
@@ -91,7 +88,7 @@ SELECT * FROM students;
 --5.- Añadir datos en la tabla de students (10 estudiantes):
 INSERT INTO students (name, address, graduated) VALUES ('Juan De Sousa', 'Passeig de Fabra i Puig, 330. Barcelona', 'true');
 
-INSERT INTO students (name, address, graduated) VALUES ('Alejandro Gil', 'Carrer de Lucà. Barcelona', 'false');
+INSERT INTO students (name, address, graduated) VALUES ('Alejandro Gil', 'Carrer de Luca. Barcelona', 'false');
 
 -- Comprobación de dos estudiantes creados:
 SELECT * FROM students;
@@ -101,36 +98,36 @@ SELECT * FROM students;
   2 | Alejandro Gil | Carrer de Lucà. Barcelona               | f
 (2 filas)
 
-INSERT INTO students (name, address, graduated) VALUES ('Ainhoa González', 'Carrer de la Diputació, 336. Barcelona', 'true');
+INSERT INTO students (name, address, graduated) VALUES ('Ainhoa Gonzalez', 'Carrer de la Diputacio, 336. Barcelona', 'true');
 
-INSERT INTO students (name, address, graduated) VALUES ('Enrique Luque', '8 Rue Garancière, Paris. Francia', 'true');
+INSERT INTO students (name, address, graduated) VALUES ('Enrique Luque', '8 Rue Garanciere, Paris. Francia', 'true');
 
-INSERT INTO students (name, address, graduated) VALUES ('José López', 'Avenida San Juan, Caracas. Venezuela', 'true');
+INSERT INTO students (name, address, graduated) VALUES ('Jose Lopez', 'Avenida San Juan, Caracas. Venezuela', 'true');
 
-INSERT INTO students (name, address, graduated) VALUES ('Anna Burgos', 'Via Favencia, Barcelona. España', 'true');
+INSERT INTO students (name, address, graduated) VALUES ('Anna Burgos', 'Via Favencia, Barcelona.', 'true');
 
 INSERT INTO students (name, address, graduated) VALUES ('Peter Pan', '27 Rue Oudinot, Paris. Francia', 'false');
 
 INSERT INTO students (name, address, graduated) VALUES ('Victoria Villegas', 'Calle Altamira, Caracas. Venezuela', 'true');
 
-INSERT INTO students (name, address, graduated) VALUES ('Josefina Soria', 'Carrer Gaudí, 4. España', 'false');
+INSERT INTO students (name, address, graduated) VALUES ('Josefina Soria', 'Carrer Gaudi, 4. Barcelona', 'false');
 
 INSERT INTO students (name, address, graduated) VALUES ('Carolina Elorza', 'Carrer de Jaume I, Viladecans. Barcelona', 'true');
 
--- Comprobación de los 10 estudiantes:
+--6.- Comprobación de los 10 estudiantes:
 SELECT * FROM students;
  id |       name        |                 address                  | graduated
 ----+-------------------+------------------------------------------+-----------
   1 | Juan De Sousa     | Passeig de Fabra i Puig, 330. Barcelona  | t
-  2 | Alejandro Gil     | Carrer de Lucà. Barcelona                | f
-  3 | Ainhoa González   | Carrer de la Diputació, 336. Barcelona   | t
-  4 | Enrique Luque     | 8 Rue Garancière, Paris. Francia         | t
-  5 | José López        | Avenida San Juan, Caracas. Venezuela     | t
-  6 | Anna Burgos       | Via Favencia, Barcelona. España          | t
   7 | Peter Pan         | 27 Rue Oudinot, Paris. Francia           | f
   8 | Victoria Villegas | Calle Altamira, Caracas. Venezuela       | t
-  9 | Josefina Soria    | Carrer Gaudí, 4. España                  | f
  10 | Carolina Elorza   | Carrer de Jaume I, Viladecans. Barcelona | t
+  5 | Jose Lopez        | Avenida San Juan, Caracas. Venezuela     | t
+  3 | Ainhoa Gonzalez   | Carrer de la Diputacio, 336. Barcelona   | t
+  2 | Alejandro Gil     | Carrer de Luca. Barcelona                | f
+  6 | Anna Burgos       | Via Favencia, Barcelona.                 | t
+  9 | Josefina Soria    | Carrer Gaudi, 4. Barcelona               | f
+  4 | Enrique Luque     | 8 Rue Garanciere, Paris. Francia         | t
 (10 filas)
 
 
