e1468b1d08cd5098ba95123fd0bcdb191688b720
Maria Victoria <marivibilbaog@gmail.com>
2021-10-27 17:09:14 +0200

    Creo base de datos y tabla llamda mentors

---
+++ week-1/mandatory/classes-db/Homework-1.sql
@@ -0,0 +1,17 @@
+--En el terminal se ejecutan los siguientes comandos:
+--1.- Creamos tabla llamada cyf_classes:
+CREATE DATABASE cyf_classes owner postgres;
+
+\q
+
+psql -U postgres cyf_hotels
+
+--2.- Se crea tabla llamda mentors:
+CREATE TABLE mentors(
+cyf_classes(# id SERIAL PRIMARY KEY,
+cyf_classes(# name VARCHAR(50) NOT NULL,
+cyf_classes(# yearsInGlasgow int,
+cyf_classes(# address VARCHAR(120),
+cyf_classes(# favProgramLanguage VARCHAR(100)
+cyf_classes(# );
+
