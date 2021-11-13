bf7a059e3f3cfee93da23d1f43a38b65ff5c8fe9
Maria Victoria <marivibilbaog@gmail.com>
2021-11-13 10:35:23 +0100

    Agrego una parte de los ejercicios week-3

--- week-2/mandatory/api/server.js
+++ week-2/mandatory/api/server.js
@@ -12,9 +12,10 @@ const pool = new Pool({
   user: "postgres",
   host: "localhost",
   database: "cyf_ecommerce",
-  password: "newPassword",
+  password: "123456",
   port: 5432,
 });
+// En donde dice password si se coloca, te carga correctamente los datos de la base de datos.
 
 app.use(express.json());
 
@@ -30,6 +31,7 @@ app.get("/suppliers", function (req, res) {
   });
 });
 
+//-- Week 3 GET --- /products
 app.get("/products", function (req, res) {
   pool.query(
     "SELECT p.*, supplier_name FROM products p JOIN suppliers s ON p.supplier_id = s.id",
@@ -38,6 +40,63 @@ app.get("/products", function (req, res) {
     }
   );
 });
+
+//-- Week 3: GET --- /customers/:customerId
+app.get("/customers/:customerId", function (req, res) {
+  const customerId = req.params.customerId;
+
+  pool
+    .query("SELECT * FROM customers WHERE id=$1", [customerId])
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
+
+//-- Week 3: POST --- /customers
+app.post("/customers", function (req, res) {
+  const newCustomerName = req.body.name;
+  const newCustomerAddress = req.body.address;
+  const newCustomerCity = req.body.city;
+  const newCustomerCountry = req.body.country;
+
+pool
+  .query("SELECT * FROM customers WHERE name=$1", [newCustomerName])
+  .then((result) => {
+    if (result.rows.length > 0) {
+      return res
+        .status(400)
+        .send("A customers with the same name already exists!");
+    } else {
+      const query =
+      "INSERT INTO customers (name, address, city, country) VALUES ($1, $2, $3, $4)";
+      pool
+        .query(query, [newCustomerName, newCustomerAddress, newCustomerCity, newCustomerCountry])
+        .then(() => res.send("Customer created!"))
+        .catch((e) => console.error(e));
+    }
+  });
+});
+
+///REVISAR
+//-- Week 3: PUT --- /customers/:customerId (name, address, city and country).
+app.put("/customers/:customerId", function (req, res) {
+  const customerId = req.params.customerId;
+  const newName = req.body.name;
+  const newAddress = req.body.address;
+  const newCity = req.body.city;
+  const newCountry = req.body.country;
+
+  pool
+    .query("PUT customers SET name=$1, address=$2, city=$3, country=$4 WHERE id=$5", [newName, newAddress, newCity, newCountry, customerId])
+    .then(() => res.send(`Customer ${customerId} updated!`))
+    .catch((e) => console.error(e));
+});
+
+
+
+
+
+
+
 
 const port = 3000;
 app.listen(port, () => console.log(`Server is running on PORT ${port}`));
